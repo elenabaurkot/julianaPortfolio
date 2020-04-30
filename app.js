4918917fb2ae9f27663a88d165e564eb290f05ba
elenabaurkot <elenabaurkot@gmail.com>
2020-04-30 02:26:38 -0400

    navbar closes when link is clicked

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li'); 
+  // const link = document.getElementById('link');
   
   burger.addEventListener('click', ()=> {
       // Toggle Nav
@@ -17,9 +18,38 @@ const navSlide = () => {
       });
       // Burger Animation
       burger.classList.toggle('toggle');
-
   });
+};
+
+
+const navClose = () => {
+  const nav = document.querySelector('.nav-links');
+  const link = document.getElementsByClassName('link');
+  const burger = document.querySelector('.burger');
+  const navLinks = document.querySelectorAll('.nav-links li'); 
+
+  for (let i = 0 ; i < link.length; i++) {
+    link[i].addEventListener('click', ()=> {
+      nav.classList.toggle('nav-active');
+      burger.classList.toggle('toggle');
+    
+  // when user clicks a link close the drop down menu
+  // link.addEventListener('click', ()=> {
+  //   nav.classList.toggle('nav-active');
+  //   burger.classList.toggle('toggle');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+          link.style.animation = '';
+      } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .1}s`;
+      }
+    });
+  })
 }
+};
+
 
 navSlide();
-
+navClose();
